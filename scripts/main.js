e22abb76ae0637182e982944e62c38bcbb34ed86
Iasen <keioff@hotmail.com>
2018-01-15 22:02:25 +0100

    Change main.js - Interactive image
    Insert code to make image change when clicking on it.
    Src values on the <img> element. Find more on dev.mozilla.org

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,13 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+/* var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!'; */
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if (mySrc === 'images/rani.png') {
+		myImage.setAttribute ('src','images/rani2.png');
+	} else {
+		myImage.setAttribute ('src','images/rani.png');
+	}
+}
